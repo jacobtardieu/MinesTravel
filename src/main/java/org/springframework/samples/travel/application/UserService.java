1dec4d2cba998300c0b222016dad6971ff7ebbc0
Lucas VANRYB <lucas@vanryb.fr>
2014-02-04 14:39:19 +0100

    Added user service into code

---
+++ src/main/java/org/springframework/samples/travel/application/UserService.java
@@ -0,0 +1,7 @@
+package org.springframework.samples.travel.application;
+
+/**
+ * Created by Lucas on 04/02/2014.
+ */
+public interface UserService {
+}
---
+++ src/main/java/org/springframework/samples/travel/application/impl/UserServiceImpl.java
@@ -0,0 +1,7 @@
+package org.springframework.samples.travel.application.impl;
+
+/**
+ * Created by Lucas on 04/02/2014.
+ */
+public class UserServiceImpl {
+}
