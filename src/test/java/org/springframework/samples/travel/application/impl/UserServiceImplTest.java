5455f4590babc3cfc548c52e4ba4dfc20c527900
Lucas VANRYB <lucas@vanryb.fr>
2014-02-04 15:05:27 +0100

    Tests completed

--- src/test/java/org/springframework/samples/travel/application/impl/UserServiceImplTest.java
+++ src/test/java/org/springframework/samples/travel/application/impl/UserServiceImplTest.java
@@ -1,21 +1,15 @@
 package org.springframework.samples.travel.application.impl;
 
-import static com.google.common.collect.Lists.newArrayList;
-import static org.fest.assertions.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
-import java.util.List;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
-import org.springframework.samples.travel.domain.model.booking.Booking;
 import org.springframework.samples.travel.domain.model.user.User;
 import org.springframework.samples.travel.domain.model.user.UserRepository;
+
+import static org.fest.assertions.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 
@@ -31,7 +25,7 @@ public class UserServiceImplTest {
 	public void shouldFindUserGivenAnUsername() {
 		// Given
 		String username = "username";
-		User user = new User();
+		User user = new User(username, "", "");
 		when(userRepository.findByUsername(username)).thenReturn(user);
 
 	}
