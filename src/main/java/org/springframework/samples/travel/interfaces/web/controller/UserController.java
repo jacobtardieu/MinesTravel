1f08815427a6d3bb3ad2fe6a264c89648c8e39a3
Clément Guillaume <clementguillaume56@gmail.com>
2014-02-04 12:30:08 +0100

    starting to change user controller

--- src/main/java/org/springframework/samples/travel/interfaces/web/controller/UserController.java
+++ src/main/java/org/springframework/samples/travel/interfaces/web/controller/UserController.java
@@ -1,14 +1,11 @@
 package org.springframework.samples.travel.interfaces.web.controller;
 
-import org.springframework.samples.travel.domain.model.booking.Booking;
-import org.springframework.samples.travel.domain.shared.SearchCriteria;
+import javax.inject.Inject;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import java.security.Principal;
-import java.util.List;
 
 /**
  * Created by calixtebonsart on 04/02/14.
@@ -16,8 +13,18 @@ import java.util.List;
 @Controller
 public class UserController {
 
+	@Inject
+	public UserController() {
+		// TODO user model
+	}
+	
     @RequestMapping(value = "/users/signup", method = RequestMethod.GET)
     public void signup() {
 
+    }
+    
+    @RequestMapping(value = "/users/profile", method = RequestMethod.GET)
+    public String profile(@PathVariable String id) {
+    	return "/users/profile";
     }
 }
