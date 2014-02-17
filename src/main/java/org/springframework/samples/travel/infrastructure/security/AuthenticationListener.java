d42ff46f42927ab102d94af02d200720389678b7
Jacob Tardieu <jacob@tardieu.net>
2014-02-17 10:14:20 +0100

    Add authenticationListener to count login attempts

---
+++ src/main/java/org/springframework/samples/travel/infrastructure/security/AuthenticationListener.java
@@ -0,0 +1,29 @@
+package org.springframework.samples.travel.infrastructure.security;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class AuthenticationListener implements AuthenticationSuccessHandler, AuthenticationFailureHandler{
+
+    int successes = 0;
+    int failures = 0;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        successes++;
+        response.sendRedirect("/hotels/search");
+    }
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+        failures++;
+        response.sendRedirect("/users/login?login_error=1");
+    }
+}
