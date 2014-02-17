3fbf31a1e4d138efcb2f98661fbf83aca1b250a3
Clément Guillaume <clementguillaume56@gmail.com>
2014-02-17 10:57:42 +0100

    list user with roles

--- src/main/java/org/springframework/samples/travel/interfaces/web/controller/UserController.java
+++ src/main/java/org/springframework/samples/travel/interfaces/web/controller/UserController.java
@@ -7,6 +7,8 @@ import javax.inject.Inject;
 
 import org.springframework.samples.travel.application.UserService;
 import org.springframework.samples.travel.domain.model.user.User;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,10 +35,17 @@ public class UserController {
     }
     
     @RequestMapping(value = "/users/admin", method = RequestMethod.GET)
-    public void list(Model model, Principal user) {
-    	if (user != null)
+    public String list(Model model, Principal user) {
+    	if (user != null && ((UsernamePasswordAuthenticationToken)user).getAuthorities().contains(new SimpleGrantedAuthority("ROLE_SUPERVISOR"))) {
     		System.out.println("AAAAAAAAAAAAAAAAA " + user);
-        List<User> users = userService.findAllUsers();
-        model.addAttribute("userList", users);
+    		System.out.println("AAAAAAAAAAAAAAAAA " + user.getClass());
+    		System.out.println(((UsernamePasswordAuthenticationToken)user).getAuthorities());
+    		System.out.println(((UsernamePasswordAuthenticationToken)user).getAuthorities().contains(new SimpleGrantedAuthority("ROLE_SUPERVISOR")));
+    		List<User> users = userService.findAllUsers();
+            model.addAttribute("userList", users);
+            return "users/admin";
+    	} else {
+    		return "home";
+    	}
     }
 }
