fb21ce6a1f2a9e12feb397249d8843280654ff0f
Jacob Tardieu <jacob@tardieu.net>
2014-02-17 12:08:50 +0100

    Add backend stats

---
+++ src/main/java/org/springframework/samples/travel/domain/model/booking/StatsRepository.java
@@ -0,0 +1,11 @@
+package org.springframework.samples.travel.domain.model.booking;
+
+public interface StatsRepository {
+
+    int getFinishedBookings();
+    int getCancelledBookings();
+
+    void incrementFinishedBookings();
+    void incrementCancelledBookings();
+
+}
